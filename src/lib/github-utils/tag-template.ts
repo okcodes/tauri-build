04663aa5fd09c768c1edeae63c08d3a9ef65b988
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-17 00:04:54 -0600

    Update docs

--- src/lib/github-utils/tag-template.ts
+++ src/lib/github-utils/tag-template.ts
@@ -23,12 +23,11 @@ type TagData = {
  * - `{DATE_ISO_8601}`: Replaced with the date provided in the `@param {Date} now` parameter, formatted in ISO 8601 format suitable for filenames.
  * - `{SHORT_SHA}`: Replaced with the first 7 characters of the Git SHA.
  *
- * @param {string} template The template string containing placeholders for tag generation.
+ * @param {string} template The template string containing placeholders for tag generation, e.g., "{NAME}-v{VERSION}-alpha-{DATE_ISO_8601}-{SHORT_SHA}".
  * @param {TagData} params Data used to replace the template placeholders.
  * @returns {string} The generated tag name with placeholders replaced by specific values.
  */
 export const tagNameFromTemplate = (template: string, { appInfo, date, gitSha }: TagData): string => {
-  // It can be used for example with this template: "{NAME}-v{VERSION}-alpha-{DATE_ISO_8601}-{SHORT_SHA}".
   return template
     .replaceAll(/\{VERSION}/g, appInfo.package.version)
     .replaceAll(/\{NAME}/g, appInfo.package.name)
