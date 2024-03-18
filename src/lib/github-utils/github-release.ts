239ebd7ca768be60bd3d248f9a2a941793ed49e2
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-18 01:45:17 -0600

    Refactor creation of release

--- src/lib/github-utils/github-release.ts
+++ src/lib/github-utils/github-release.ts
@@ -16,11 +16,10 @@ type Params = {
 }
 
 export const getOrCreateGitHubRelease = async ({ githubToken, repo, owner, tag, sha, prerelease, draft }: Params): Promise<void> => {
-  const octokit = new Octokit({ auth: githubToken })
-
   try {
     // First try to get release by tag. If not found, create it.
     core.debug(`Will get existing release with tag "${tag}"`)
+    const octokit = new Octokit({ auth: githubToken })
     const existingRelease = await octokit.repos.getReleaseByTag({ owner, repo, tag })
     core.debug(`Did get existing release with tag "${tag}". ID: "${existingRelease.data.id}"`)
   } catch (getReleaseError) {
@@ -30,19 +29,23 @@ export const getOrCreateGitHubRelease = async ({ githubToken, repo, owner, tag, 
     }
 
     // Release not found, create it.
-    core.debug(`Release with tag "${tag}" not found, will create it.`)
-
-    // TODO: Create a tag manually so it's signed.
-    const createReleaseResponse = await octokit.repos.createRelease({
-      owner,
-      repo,
-      tag_name: tag,
-      target_commitish: sha,
-      name: `Release ${tag}`,
-      body: `# Release \`${tag}\``,
-      draft,
-      prerelease,
-    } as CreateReleaseParams)
-    console.log(`Did create release with tag "${tag}". ID: ${createReleaseResponse.data.id}`)
+    core.debug(`Release with tag "${tag}" not found.`)
+    await createGitHubRelease({ githubToken, repo, owner, tag, sha, prerelease, draft })
   }
+}
+
+export const createGitHubRelease = async ({ githubToken, repo, owner, tag, sha, prerelease, draft }: Params): Promise<void> => {
+  core.debug(`Will create will release with tag "${tag}" it.`)
+  const octokit = new Octokit({ auth: githubToken })
+  const createReleaseResponse = await octokit.repos.createRelease({
+    owner,
+    repo,
+    tag_name: tag,
+    target_commitish: sha,
+    name: `Release ${tag}`,
+    body: `# Release \`${tag}\``,
+    draft,
+    prerelease,
+  } as CreateReleaseParams)
+  console.log(`Did create release with tag "${tag}". ID: ${createReleaseResponse.data.id}`)
 }
