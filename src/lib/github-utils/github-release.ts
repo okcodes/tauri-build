472afd5e11b668683b43b9fe60fbac0378d5d1ee
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-18 01:56:31 -0600

    Create signed tag

--- src/lib/github-utils/github-release.ts
+++ src/lib/github-utils/github-release.ts
@@ -36,7 +36,37 @@ export const getOrCreateGitHubRelease = async ({ githubToken, repo, owner, tag, 
 
 export const createGitHubRelease = async ({ githubToken, repo, owner, tag, sha, prerelease, draft }: Params): Promise<void> => {
   core.debug(`Will create will release with tag "${tag}" it.`)
+
   const octokit = new Octokit({ auth: githubToken })
+
+  // Create tag manually so it get signed.
+  try {
+    console.log(`Will create tag "${tag}" in commit "${sha}"`)
+    const createTagResponse = await octokit.git.createTag({
+      owner,
+      repo,
+      tag,
+      message: tag,
+      object: sha,
+      type: 'commit',
+    } as RestEndpointMethodTypes['git']['createTag']['parameters'])
+    console.log(`Did create tag "${tag}" in commit "${sha}". Tag sha: "${createTagResponse.data.sha}".`)
+
+    // Create a reference for the tag
+    console.log(`Will create reference to tag "${tag}".`)
+    const referenceResponse = await octokit.git.createRef({
+      owner,
+      repo,
+      ref: `refs/tags/${tag}`,
+      sha: createTagResponse.data.sha, // Is this sha different from the commit sha?
+    })
+    console.log(`Did create reference to tag "${tag}".`, referenceResponse)
+  } catch (createTagError) {
+    console.error(`Cannot create tag "${tag}" on commit "${sha}": ${(createTagError as Error).message}`, createTagError)
+    // Don't throw. Creating a tag is not required, if a release is created the tag is created automatically.
+    // The manual tag creation is just for creating a signed tag but that's not a required step.
+  }
+
   const createReleaseResponse = await octokit.repos.createRelease({
     owner,
     repo,
