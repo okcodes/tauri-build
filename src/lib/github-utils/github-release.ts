b72a48dc999b99d3e7413253ba469b316c4d090f
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-18 02:18:22 -0600

    Don't manually create tag

--- src/lib/github-utils/github-release.ts
+++ src/lib/github-utils/github-release.ts
@@ -1,5 +1,4 @@
 import { Octokit } from '@octokit/rest'
-import * as core from '@actions/core'
 
 import type { RestEndpointMethodTypes } from '@octokit/rest'
 
@@ -16,12 +15,12 @@ type Params = {
 }
 
 export const getOrCreateGitHubRelease = async ({ githubToken, repo, owner, tag, sha, prerelease, draft }: Params): Promise<void> => {
+  const octokit = new Octokit({ auth: githubToken })
   try {
     // First try to get release by tag. If not found, create it.
-    core.info(`Will get existing release with tag "${tag}"`)
-    const octokit = new Octokit({ auth: githubToken })
+    console.log(`Will get existing release with tag "${tag}"`)
     const existingRelease = await octokit.repos.getReleaseByTag({ owner, repo, tag })
-    core.info(`Did get existing release with tag "${tag}". ID: "${existingRelease.data.id}"`)
+    console.log(`Did get existing release with tag "${tag}". ID: "${existingRelease.data.id}"`)
   } catch (getReleaseError) {
     // If error is not 404, it's an unknown error.
     if ((getReleaseError as any).status !== 404) {
@@ -29,53 +28,17 @@ export const getOrCreateGitHubRelease = async ({ githubToken, repo, owner, tag, 
     }
 
     // Release not found, create it.
-    core.info(`Release with tag "${tag}" not found.`)
-    await createGitHubRelease({ githubToken, repo, owner, tag, sha, prerelease, draft })
-  }
-}
-
-export const createGitHubRelease = async ({ githubToken, repo, owner, tag, sha, prerelease, draft }: Params): Promise<void> => {
-  core.info(`Will create will release with tag "${tag}" it.`)
-
-  const octokit = new Octokit({ auth: githubToken })
-
-  // Create tag manually so it get signed.
-  try {
-    console.log(`Will create tag "${tag}" in commit "${sha}"`)
-    const createTagResponse = await octokit.git.createTag({
+    console.log(`Release with tag "${tag}" not found. Will create it.`)
+    const createReleaseResponse = await octokit.repos.createRelease({
       owner,
       repo,
-      tag,
-      message: tag,
-      object: sha,
-      type: 'commit',
-    } as RestEndpointMethodTypes['git']['createTag']['parameters'])
-    console.log(`Did create tag "${tag}" in commit "${sha}". Tag sha: "${createTagResponse.data.sha}".`)
-
-    // Create a reference for the tag
-    console.log(`Will create reference to tag "${tag}".`)
-    const referenceResponse = await octokit.git.createRef({
-      owner,
-      repo,
-      ref: `refs/tags/${tag}`,
-      sha: createTagResponse.data.sha, // Is this sha different from the commit sha?
-    })
-    console.log(`Did create reference to tag "${tag}".`, referenceResponse)
-  } catch (createTagError) {
-    console.error(`Cannot create tag "${tag}" on commit "${sha}": ${(createTagError as Error).message}`, createTagError)
-    // Don't throw. Creating a tag is not required, if a release is created the tag is created automatically.
-    // The manual tag creation is just for creating a signed tag but that's not a required step.
+      tag_name: tag,
+      target_commitish: sha,
+      name: `Release ${tag}`,
+      body: `Tag \`${tag}\``,
+      draft,
+      prerelease,
+    } as CreateReleaseParams)
+    console.log(`Did create release with tag "${tag}". ID: ${createReleaseResponse.data.id}`)
   }
-
-  const createReleaseResponse = await octokit.repos.createRelease({
-    owner,
-    repo,
-    tag_name: tag,
-    target_commitish: sha,
-    name: `Release ${tag}`,
-    body: `# Release \`${tag}\``,
-    draft,
-    prerelease,
-  } as CreateReleaseParams)
-  console.log(`Did create release with tag "${tag}". ID: ${createReleaseResponse.data.id}`)
 }
