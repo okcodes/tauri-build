636b136a9f610e0e317c9e3eb81c573e1197b5da
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-19 14:38:45 -0600

    Use { stdio: 'inherit' }

--- src/lib/command-utils/command-utils.ts
+++ src/lib/command-utils/command-utils.ts
@@ -1,6 +1,6 @@
-export const executeCommand = async (command: string, options?: { cwd?: string }): Promise<{ stdout: string; stderr: string }> => {
+export const executeCommand = async (command: string, options: { cwd?: string } = {}): Promise<{ stdout: string; stderr: string }> => {
   const { execaCommand } = await import('execa')
-  const subprocess = execaCommand(command, options)
+  const subprocess = execaCommand(command, { ...options, stdio: 'inherit' })
   // Realtime log subprocess output
   subprocess?.stdout?.on('data', chunk => console.log(chunk.toString()))
   subprocess?.stderr?.on('data', chunk => console.error(chunk.toString()))
