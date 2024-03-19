792e3377f0a75fcce9de4774319dfa221bfce2af
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-19 02:09:14 -0600

    Better options parse with yargs

--- src/lib/tauri-utils/tauri-builder.test.ts
+++ src/lib/tauri-utils/tauri-builder.test.ts
@@ -6,6 +6,7 @@ let executeCommandMock: jest.SpiedFunction<typeof commandUtils.executeCommand>
 
 const buildMock = jest.spyOn(tauriBuilder, 'build')
 const targetFromOptionsMock = jest.spyOn(tauriBuilder, 'targetFromOptions')
+const parseArgsStringMock = jest.spyOn(tauriBuilder, 'parseArgsString')
 
 describe('build', () => {
   beforeEach(() => {
@@ -21,23 +22,50 @@ describe('build', () => {
     ['project-with-npm', 'npm'],
   ])('Project "%s" uses "%s" as package manager', async (projectDir, expectedPackageManager) => {
     const tauriContext = path.join(__dirname, 'test-files', projectDir)
-    await tauriBuilder.build(tauriContext, "     --target  aarch64-apple-darwin   --bundles   'app,dmg,updater'   ") // Build options supports trimming repeated spaces.
+    await tauriBuilder.build(tauriContext, '     --target  aarch64-apple-darwin   --bundles "app,dmg,updater"  ') // Build options supports trimming repeated spaces.
     expect(buildMock).toHaveReturned()
     expect(executeCommandMock).toHaveBeenNthCalledWith(1, expectedPackageManager, ['install'], { cwd: tauriContext })
-    expect(executeCommandMock).toHaveBeenNthCalledWith(2, expectedPackageManager, ['tauri', 'build', '--target', 'aarch64-apple-darwin', '--bundles', "'app,dmg,updater'"], { cwd: tauriContext })
+    // Note how the "bundles" flag is special, and it will always surround all the bundles with single quotes and will remove spaces between them.
+    expect(executeCommandMock).toHaveBeenNthCalledWith(2, expectedPackageManager, ['tauri', 'build', '--target', "'aarch64-apple-darwin'", '--bundles', "'app,dmg,updater'"], { cwd: tauriContext })
   })
 })
 
 describe('targetFromOptions', () => {
   test.each([
-    ["     --target  aarch64-apple-darwin   --bundles   'app,dmg,updater'   ", 'aarch64-apple-darwin'],
-    ["     -t  x86_64-pc-windows-msvc   --bundles   'app,dmg,updater'   ", 'x86_64-pc-windows-msvc'],
-    ['-t', void 0],
-    ['--target', void 0],
-    ['', void 0],
-    ["--bundles 'app,dmg,updater'", void 0],
+    ["     --target  aarch64-apple-darwin   --bundles   'app,dmg,updater'   ", "'aarch64-apple-darwin'"], // Target set via full name
+    ["     -t  x86_64-pc-windows-msvc   --bundles   'app,dmg,updater'   ", "'x86_64-pc-windows-msvc'"], // Target set via alias
+    ['', void 0], // No flags set at all
+    ["--bundles 'app,dmg,updater'", void 0], // Target not set, other flags set
   ])('Parse options "%s" should contain target "%s"', (options, target) => {
     tauriBuilder.targetFromOptions(options)
     expect(targetFromOptionsMock).toHaveReturnedWith(target)
+  })
+})
+
+describe('parseArgsString', () => {
+  test.each([
+    {
+      input: '--option1 value1 --option2 value2',
+      expected: ['--option1', "'value1'", '--option2', "'value2'"],
+    },
+    {
+      input: "--option-with-spaces 'value with spaces' --option2 value2",
+      expected: ['--option-with-spaces', "'value with spaces'", '--option2', "'value2'"],
+    },
+    {
+      input: '--booleanOption',
+      expected: ['--booleanOption', "'true'"],
+    },
+    {
+      input: "-a shorthand -b 'another value'",
+      expected: ['-a', "'shorthand'", '-b', "'another value'"],
+    },
+    {
+      input: "--mixed 'mixed value' --numeric 12345",
+      expected: ['--mixed', "'mixed value'", '--numeric', "'12345'"],
+    },
+  ])('correctly parses input "$input" into expected output', ({ input, expected }) => {
+    tauriBuilder.parseArgsString(input)
+    expect(parseArgsStringMock).toHaveReturnedWith(expected)
   })
 })
--- src/lib/tauri-utils/tauri-builder.ts
+++ src/lib/tauri-utils/tauri-builder.ts
@@ -1,5 +1,6 @@
 import { executeCommand } from '../command-utils/command-utils'
 import { getPackageManager } from './tauri-utils'
+import yargs from 'yargs'
 
 const RESET = '\x1b[0m'
 const GREEN = '\x1b[32m'
@@ -17,7 +18,7 @@ export const build = async (tauriContext: string, buildOptions: string) => {
     console.log(`${GREEN}Did install node dependencies${RESET}`, { packageManager })
 
     // Build tauri app
-    args = ['tauri', 'build', ...cleanOptions(buildOptions)]
+    args = ['tauri', 'build', ...parseArgsString(buildOptions)]
     console.log(`${GREEN}Will build tauri project${RESET}`, { packageManager })
     await executeCommand(packageManager, args, { cwd: tauriContext })
     console.log(`${GREEN}Did build tauri project${RESET}`, { packageManager })
@@ -26,12 +27,23 @@ export const build = async (tauriContext: string, buildOptions: string) => {
   }
 }
 
-export const cleanOptions = (options: string): string[] => {
-  return options.replace(/\s+/g, ' ').split(' ').filter(Boolean)
+/**
+ * Parses a command line arguments string into a flat array of keys and values.
+ *
+ * @param {string} argsString - The raw string of command line arguments.
+ * @returns {string[]} An array containing the parsed keys and values, where each key is immediately followed by its corresponding value. The values are always surrounded by single quotes.
+ *
+ * Example:
+ * Input: "--name John Doe --age 30"
+ * Output: ["--name", "'John Doe'", "age", "'30'"]
+ */
+export const parseArgsString = (argsString: string): string[] => {
+  const { $0, _, ...params } = yargs().parserConfiguration({ 'camel-case-expansion': false, 'parse-numbers': false, 'boolean-negation': false }).strict(false).help(false).version(false).parseSync(argsString)
+  return Object.entries(params as Record<string, unknown>).flatMap(([key, value]) => [key.length === 1 ? `-${key}` : `--${key}`, `'${value}'`])
 }
 
 export const targetFromOptions = (options: string): string | undefined => {
-  const optionsArray = cleanOptions(options)
+  const optionsArray = parseArgsString(options)
   const index = optionsArray.findIndex(_ => _ === '--target' || _ === '-t')
 
   if (index === -1) {
