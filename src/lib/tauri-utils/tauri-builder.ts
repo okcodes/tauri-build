2ed1f03c9a01f09a2a69e7d1a33767309c81c6da
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-19 00:38:27 -0600

    Color logs of build process

--- src/lib/tauri-utils/tauri-builder.ts
+++ src/lib/tauri-utils/tauri-builder.ts
@@ -1,15 +1,29 @@
 import { executeCommand } from '../command-utils/command-utils'
 import { getPackageManager } from './tauri-utils'
 
+const RESET = '\x1b[0m'
+const GREEN = '\x1b[32m'
+const RED = '\x1b[31m'
+
 export const build = async (tauriContext: string, buildOptions: string) => {
   const packageManager = getPackageManager(tauriContext)
-  console.log('Will install node dependencies', { packageManager })
-  await executeCommand(packageManager, ['install'], { cwd: tauriContext })
-  console.log('Did install node dependencies', { packageManager })
-  const options = ['tauri', 'build', ...cleanOptions(buildOptions)]
-  console.log('Will build tauri project', { packageManager, options })
-  await executeCommand(packageManager, options, { cwd: tauriContext })
-  console.log('Did build tauri project', { packageManager, options })
+  let args: string[] = []
+
+  try {
+    // Install node deps
+    console.log(`${GREEN}Will install node dependencies${RESET}`, { packageManager })
+    args = ['install']
+    await executeCommand(packageManager, args, { cwd: tauriContext })
+    console.log(`${GREEN}Did install node dependencies${RESET}`, { packageManager })
+
+    // Build tauri app
+    args = ['tauri', 'build', ...cleanOptions(buildOptions)]
+    console.log(`${GREEN}Will build tauri project${RESET}`, { packageManager })
+    await executeCommand(packageManager, args, { cwd: tauriContext })
+    console.log(`${GREEN}Did build tauri project${RESET}`, { packageManager })
+  } catch (error) {
+    console.log(`${RED}Build failed when running command${RESET}`, { packageManager, args })
+  }
 }
 
 export const cleanOptions = (options: string): string[] => {
