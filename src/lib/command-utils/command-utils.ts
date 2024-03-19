f1a2a22e9c7747aea1ec98203b83bff1fd74c03d
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-19 00:24:22 -0600

    Realtime log subprocess

--- src/lib/command-utils/command-utils.ts
+++ src/lib/command-utils/command-utils.ts
@@ -1,4 +1,8 @@
 export const executeCommand = async (file: string, args?: readonly string[], options?: { cwd?: string }): Promise<{ stdout: string; stderr: string }> => {
   const { execa } = await import('execa')
-  return execa(file, args, options)
+  const subprocess = execa(file, args, options)
+  // Realtime log subprocess output
+  subprocess?.stdout?.on('data', chunk => console.log(chunk.toString()))
+  subprocess?.stderr?.on('data', chunk => console.error(chunk.toString()))
+  return subprocess
 }
