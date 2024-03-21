7295f0743baad8db5957fcbe172885867fef07c9
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-21 14:21:12 -0600

    Don't use draft

--- dist/index.js
+++ dist/index.js
@@ -33320,8 +33320,8 @@ const getOrCreateGitHubRelease = async ({ githubToken, repo, owner, tag, sha, pr
     try {
         // First try to get release by tag. If not found, create it.
         console.log(`Will get existing release with tag "${tag}"`, { owner, repo, tag });
-        const existingRelease = await octokit.repos.getReleaseByTag({ owner, repo, tag });
-        console.log(`Did get existing release with tag "${tag}". ID: "${existingRelease.data.id}"`);
+        const release = await octokit.repos.getReleaseByTag({ owner, repo, tag });
+        console.log(`Did get existing release with tag "${tag}".`, { owner, repo, tag, release: release.data });
     }
     catch (error) {
         // If error is not 404, it's an unknown error.
@@ -33341,7 +33341,7 @@ const getOrCreateGitHubRelease = async ({ githubToken, repo, owner, tag, sha, pr
             draft,
             prerelease,
         });
-        console.log(`Did create release with tag "${tag}". ID: ${createReleaseResponse.data.id}`, { owner, repo, tag, sha, draft, prerelease });
+        console.log(`Did create release with tag "${tag}"`, { owner, repo, tag, sha, draft, prerelease, release: createReleaseResponse.data });
     }
     finally {
         core.endGroup();
@@ -33713,7 +33713,7 @@ const uploadAppToGithub = async ({ rustTarget, appName, tauriContext, expectedAr
         const octokit = new rest_1.Octokit({ auth: githubToken });
         console.log('Will get release by tag', { owner, repo, tag });
         const release = await octokit.repos.getReleaseByTag({ owner, repo, tag });
-        console.log('Did get release by tag', { owner, repo, tag, releaseId: release.data.id });
+        console.log('Did get release by tag', { owner, repo, tag, release: release.data });
         const uploadUrl = release.data.upload_url;
         console.log(`Will upload ${artifacts.length} artifacts`, artifacts);
         for (const artifact of artifacts) {
