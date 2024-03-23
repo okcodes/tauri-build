40bf074d17e9072e71f09e9cf5a87f91d38daf09
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-22 19:40:36 -0600

    Add unit tests for edge cases

--- src/lib/tauri-utils/tauri-updater-assembler.test.ts
+++ src/lib/tauri-utils/tauri-updater-assembler.test.ts
@@ -131,6 +131,108 @@ describe('assembleSemiUpdater', () => {
         },
       },
     },
+    {
+      description: 'app build for windows + macOS (prefer with fallback)',
+      appVersion: '0.0.0',
+      pubDate: new Date('2024-03-16T10:05:30Z').toString(),
+      assetNames: [
+        // macOS Silicon (darwin-aarch64)
+        'aarch64-apple-darwin.xxx_0.0.18_aarch64.updater.app.tar.gz.sig',
+        // macOS Intel (darwin-x86_64)
+        'x86_64-apple-darwin.xxx_0.0.18_x64.updater.app.tar.gz.sig',
+        // macOS Universal (darwin-aarch64, darwin-x86_64)
+        // 'universal-apple-darwin.xxx_0.0.18_universal.updater.app.tar.gz.sig', // Disable universal
+        // Windows 64 (windows-x86_64)
+        // 'x86_64-pc-windows-msvc.xxx_0.0.18_x64-setup.updater.nsis.zip.sig', // Disable NSIS
+        'x86_64-pc-windows-msvc.xxx_0.0.18_x64_en-US.updater.msi.zip.sig',
+        // Windows 32 (windows-i686)
+        // 'i686-pc-windows-msvc.xxx_0.0.18_x86-setup.updater.nsis.zip.sig', // Disable NSIS
+        'i686-pc-windows-msvc.xxx_0.0.18_x86_en-US.updater.msi.zip.sig',
+        // Windows ARM64 (windows-aarch64)
+        'aarch64-pc-windows-msvc.xxx_0.0.18_arm64-setup.updater.nsis.zip.sig',
+      ],
+      preferUniversal: true,
+      preferNsis: true,
+      expected: {
+        version: '0.0.0',
+        notes: `Version 0.0.0 brings enhancements and bug fixes for improved performance and stability.`,
+        pub_date: new Date('2024-03-16T10:05:30Z').toString(),
+        platformsPlaceholder: {
+          // Preferring universal, but it's not available fallbacks to platform-specific signatures
+          'darwin-aarch64': 'aarch64-apple-darwin.xxx_0.0.18_aarch64.updater.app.tar.gz.sig',
+          'darwin-x86_64': 'x86_64-apple-darwin.xxx_0.0.18_x64.updater.app.tar.gz.sig',
+          'windows-aarch64': 'aarch64-pc-windows-msvc.xxx_0.0.18_arm64-setup.updater.nsis.zip.sig',
+          // Preferring NSIS, but it's not available, fallbacks to MSI
+          'windows-i686': 'i686-pc-windows-msvc.xxx_0.0.18_x86_en-US.updater.msi.zip.sig',
+          'windows-x86_64': 'x86_64-pc-windows-msvc.xxx_0.0.18_x64_en-US.updater.msi.zip.sig',
+        },
+      },
+    },
+    {
+      description: 'app build for windows + macOS (not prefer with fallback)',
+      appVersion: '0.0.0',
+      pubDate: new Date('2024-03-16T10:05:30Z').toString(),
+      assetNames: [
+        // macOS Silicon (darwin-aarch64)
+        // 'aarch64-apple-darwin.xxx_0.0.18_aarch64.updater.app.tar.gz.sig', // Disable platform-specific
+        // macOS Intel (darwin-x86_64)
+        // 'x86_64-apple-darwin.xxx_0.0.18_x64.updater.app.tar.gz.sig', // Disable platform-specific
+        // macOS Universal (darwin-aarch64, darwin-x86_64)
+        'universal-apple-darwin.xxx_0.0.18_universal.updater.app.tar.gz.sig',
+        // Windows 64 (windows-x86_64)
+        'x86_64-pc-windows-msvc.xxx_0.0.18_x64-setup.updater.nsis.zip.sig',
+        // 'x86_64-pc-windows-msvc.xxx_0.0.18_x64_en-US.updater.msi.zip.sig', // Disable MSI
+        // Windows 32 (windows-i686)
+        'i686-pc-windows-msvc.xxx_0.0.18_x86-setup.updater.nsis.zip.sig',
+        // 'i686-pc-windows-msvc.xxx_0.0.18_x86_en-US.updater.msi.zip.sig', // Disable MSI
+        // Windows ARM64 (windows-aarch64)
+        'aarch64-pc-windows-msvc.xxx_0.0.18_arm64-setup.updater.nsis.zip.sig',
+      ],
+      preferUniversal: false,
+      preferNsis: false,
+      expected: {
+        version: '0.0.0',
+        notes: `Version 0.0.0 brings enhancements and bug fixes for improved performance and stability.`,
+        pub_date: new Date('2024-03-16T10:05:30Z').toString(),
+        platformsPlaceholder: {
+          // Preferring platform-specific, but only universal is available, use universal
+          'darwin-aarch64': 'universal-apple-darwin.xxx_0.0.18_universal.updater.app.tar.gz.sig',
+          'darwin-x86_64': 'universal-apple-darwin.xxx_0.0.18_universal.updater.app.tar.gz.sig',
+          'windows-aarch64': 'aarch64-pc-windows-msvc.xxx_0.0.18_arm64-setup.updater.nsis.zip.sig',
+          // Preferring MSI, but it's not available, fallbacks to NSIS
+          'windows-i686': 'i686-pc-windows-msvc.xxx_0.0.18_x86-setup.updater.nsis.zip.sig',
+          'windows-x86_64': 'x86_64-pc-windows-msvc.xxx_0.0.18_x64-setup.updater.nsis.zip.sig',
+        },
+      },
+    },
+    {
+      description: 'empty platforms block if no signatures available (no prefer)',
+      appVersion: '1.2.3',
+      pubDate: new Date('2025-01-12T05:00:03Z').toString(),
+      assetNames: [],
+      preferUniversal: false,
+      preferNsis: false,
+      expected: {
+        version: '1.2.3',
+        notes: `Version 1.2.3 brings enhancements and bug fixes for improved performance and stability.`,
+        pub_date: new Date('2025-01-12T05:00:03Z').toString(),
+        platformsPlaceholder: {},
+      },
+    },
+    {
+      description: 'empty platforms block if no signatures available (prefer)',
+      appVersion: '4.5.6',
+      pubDate: new Date('2014-01-02T22:44:04Z').toString(),
+      assetNames: [],
+      preferUniversal: true,
+      preferNsis: true,
+      expected: {
+        version: '4.5.6',
+        notes: `Version 4.5.6 brings enhancements and bug fixes for improved performance and stability.`,
+        pub_date: new Date('2014-01-02T22:44:04Z').toString(),
+        platformsPlaceholder: {},
+      },
+    },
   ]
   test.each(testCases)('$description preferUniversal: $preferUniversal, preferNsis: $preferNsis, expects: $expected', ({ appVersion, pubDate, assetNames, preferUniversal, preferNsis, expected }) => {
     const updater = assembleSemiUpdater({
