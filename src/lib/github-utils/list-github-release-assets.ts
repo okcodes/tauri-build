3c4d0753e07b9ceafd278391e52ca3b640dc2a9a
okcodes <80954089+okcodes@users.noreply.github.com>
2024-03-23 22:10:12 -0600

    Add TODOs

--- src/lib/github-utils/list-github-release-assets.ts
+++ src/lib/github-utils/list-github-release-assets.ts
@@ -2,6 +2,8 @@ import { Octokit } from '@octokit/rest'
 
 export const listGithubReleaseAssets = async ({ githubToken, owner, repo, releaseId }: { githubToken: string; owner: string; repo: string; releaseId: number }): Promise<Record<string, string>> => {
   try {
+    // TODO: Enable substituting the URL in the updater so we can use our S3 URL. Don't make this action dependent on S3, so just a URL_TEMPLATE will do it.
+    // TODO: Auto pagination. Put hard limit of 500 assets.
     const octokit = new Octokit({ auth: githubToken })
     const response = await octokit.repos.listReleaseAssets({ owner, repo, release_id: releaseId, per_page: 10 })
     console.log('Listed release assets', { owner, repo, releaseId, response })
